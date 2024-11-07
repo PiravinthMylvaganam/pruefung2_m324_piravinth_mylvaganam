cc87fbb0b8a7c5c1d800f7d281b94e98a1fa1a50
Piravinth Mylvaganam <piravinth_mylvagana@sluz.ch>
2024-11-07 09:57:09 +0100

    Task 3: Fixed Linter Error

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import { generate } from 'random-words';
 import chalk from 'chalk';
+
 export const sum = (a, b) => {
   return a + b;
 };
 
 const word = generate();
-const word2 = generate();
+// Entferne die ungenutzte Variable word2
+// const word2 = generate(); // Diese Zeile wird entfernt.
+
 console.log(
   `Hello!!! 
   The ${chalk.redBright('random word')} of today is: ${chalk.bgRed(word)}. 
